b2bf762315f6b456be590f754745720af0c5a123
Abdul Rahim <abdul.rahim@paysyslabs.com>
2025-11-17 12:00:12 +0500

    feat: removed the authGuard from handleHealthCheck

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { TazamaAuthGuard } from './auth/tazama-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @UseGuards(TazamaAuthGuard)
   handleHealthCheck(): string {
     return this.appService.handleHealthCheck().status;
   }
