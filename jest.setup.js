cd3abf42f72423dc253a00f9a0d4870e228db910
abdul-rahim-psl <abdul.rahim@paysyslabs.com>
2025-12-08 12:17:04 +0500

    fix: fixed the jest.setup

--- jest.setup.js
+++ jest.setup.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 // Load test environment variables from .env.test file
 require('dotenv').config({
-  path: path.resolve(process.cwd(), '.env'),
+  path: path.resolve(process.cwd(), '.env.test'),
 });
 
 // Ensure NODE_ENV is set to test (this should already be set in .env.test but ensuring it here)
